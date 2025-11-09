cea7e19eece245498fc0951274e66d10b70d2f9a
yumina0616 <youmina0616@gmail.com>
2025-11-10 00:09:36 +0900

    chore: stop tracking root node_modules (use .gitignore)

--- node_modules/recharts/types/state/selectors/arrayEqualityCheck.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function arrayEqualityCheck<T>(a: ReadonlyArray<T>, b: ReadonlyArray<T>): boolean;
--- node_modules/recharts/types/util/getRadiusAndStrokeWidthFromDot.d.ts
+++
@@ -1,5 +0,0 @@
-import { ActiveDotType } from './types';
-export declare function getRadiusAndStrokeWidthFromDot(dot: ActiveDotType): {
-    r: number;
-    strokeWidth: number;
-};
